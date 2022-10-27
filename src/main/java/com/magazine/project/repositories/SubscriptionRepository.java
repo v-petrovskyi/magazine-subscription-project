9e03a332f8d811c5f23f8abf39ff74ccb30a7f2f
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-10-27 22:07:17 +0300

    add SubscriptionRepository.java

---
+++ src/main/java/com/magazine/project/repositories/SubscriptionRepository.java
@@ -0,0 +1,9 @@
+package com.magazine.project.repositories;
+
+import com.magazine.project.entity.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+}

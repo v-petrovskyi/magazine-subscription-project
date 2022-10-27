a147c976871f98873078032501b399f4bb3c3226
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-10-27 21:52:34 +0300

    add entity Subscription.java

---
+++ src/main/java/com/magazine/project/entity/Subscription.java
@@ -0,0 +1,36 @@
+package com.magazine.project.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "subscriptions")
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "subscription_date", nullable = false)
+    private LocalDateTime subscriptionDate;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "magazine_id")
+    private Magazine magazine;
+
+    @Column
+    private double price;
+    }

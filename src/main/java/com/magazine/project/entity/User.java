c630a9d063ebc1732ce05d2959448c554bc9f79a
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-10-27 21:35:05 +0300

    add entity User.java

---
+++ src/main/java/com/magazine/project/entity/User.java
@@ -0,0 +1,50 @@
+package com.magazine.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "The 'name' cannot be empty")
+    @Column(name = "user_name", unique = true, nullable = false)
+    @Size(min = 3)
+    private String userName;
+
+    @Column(name = "email", unique = true, nullable = false)
+    @Email
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @OneToOne
+    private UserInfo userInfo;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinTable(name = "user_magazine",
+            joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "magazine_id"))
+    private List<Magazine> magazines;
+
+
+
+}

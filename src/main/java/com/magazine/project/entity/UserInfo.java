90bd5e924782650821367804dd682d9796c4b05b
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-10-27 21:53:49 +0300

    UserInfo.java fix

--- src/main/java/com/magazine/project/entity/UserInfo.java
+++ src/main/java/com/magazine/project/entity/UserInfo.java
@@ -1,4 +1,24 @@
 package com.magazine.project.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "user_info")
 public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
 }

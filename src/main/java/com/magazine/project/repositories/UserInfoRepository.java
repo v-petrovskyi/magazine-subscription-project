66ce18bbd998c71db50840ecaf5d6232e4be153a
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-10-27 22:08:27 +0300

    add UserInfoRepository.java

---
+++ src/main/java/com/magazine/project/repositories/UserInfoRepository.java
@@ -0,0 +1,9 @@
+package com.magazine.project.repositories;
+
+import com.magazine.project.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+}

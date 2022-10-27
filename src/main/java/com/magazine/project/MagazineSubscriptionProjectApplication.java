7cb1207da53b8bb320764de4aad7096436f375ce
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-10-27 20:55:50 +0300

    Initial commit

---
+++ src/main/java/com/magazine/project/MagazineSubscriptionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.magazine.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineSubscriptionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazineSubscriptionProjectApplication.class, args);
+    }
+
+}

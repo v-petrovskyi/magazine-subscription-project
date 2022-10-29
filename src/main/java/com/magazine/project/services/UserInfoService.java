7e9382c6e6906fcc0d848333c5c7a41539c63036
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-10-29 18:39:47 +0300

    UserInfoServiceImpl.java.java implemented

--- src/main/java/com/magazine/project/services/UserInfoService.java
+++ src/main/java/com/magazine/project/services/UserInfoService.java
@@ -5,6 +5,6 @@ import com.magazine.project.entity.UserInfo;
 public interface UserInfoService {
     boolean add(UserInfo userInfo);
     UserInfo getById(long id);
-    boolean update(long id, UserInfo userInfo);
+    boolean update(UserInfo userInfo);
     boolean delete(long id);
 }
--- src/main/java/com/magazine/project/services/impl/UserInfoServiceImpl.java
+++ src/main/java/com/magazine/project/services/impl/UserInfoServiceImpl.java
@@ -1,26 +1,53 @@
 package com.magazine.project.services.impl;
 
 import com.magazine.project.entity.UserInfo;
+import com.magazine.project.repositories.UserInfoRepository;
 import com.magazine.project.services.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
 public class UserInfoServiceImpl implements UserInfoService {
+
+    private final UserInfoRepository userInfoRepository;
+
+    public UserInfoServiceImpl(UserInfoRepository userInfoRepository) {
+        this.userInfoRepository = userInfoRepository;
+    }
+
     @Override
     public boolean add(UserInfo userInfo) {
-        return false;
+        UserInfo saved = userInfoRepository.save(userInfo);
+        try {
+            saved.getId();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public UserInfo getById(long id) {
-        return null;
+        Optional<UserInfo> optionalUserInfo = userInfoRepository.findById(id);
+        try {
+            return optionalUserInfo.orElseThrow();
+        } catch (NoSuchElementException e) {
+            throw new EntityNotFoundException("UserInfo with id " + id + " not found");
+        }
     }
 
     @Override
-    public boolean update(long id, UserInfo userInfo) {
-        return false;
+    public boolean update(UserInfo userInfo) {
+        return add(userInfo);
     }
 
     @Override
     public boolean delete(long id) {
-        return false;
+        userInfoRepository.delete(getById(id));
+        return userInfoRepository.findById(id).isEmpty();
     }
 }

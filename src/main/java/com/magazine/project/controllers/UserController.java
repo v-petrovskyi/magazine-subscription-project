5d5a9eff539711ab698d5590483e737906596fff
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-11-16 22:02:29 +0200

    info.html shows all subscriptions

--- src/main/java/com/magazine/project/controllers/UserController.java
+++ src/main/java/com/magazine/project/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.magazine.project.controllers;
 
 import com.magazine.project.entity.Magazine;
+import com.magazine.project.entity.Subscription;
 import com.magazine.project.entity.User;
 import com.magazine.project.security.UserDetailsSecurity;
 import com.magazine.project.services.MagazineService;
@@ -10,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Slf4j
@@ -27,8 +31,12 @@ public class UserController {
     }
 
     @GetMapping("/info")
-    public String showUserPage() {
-
+    public String showUserPage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsSecurity principal = (UserDetailsSecurity) authentication.getPrincipal();
+        User user = principal.getUser();
+        List<Subscription> allByUser = subscriptionService.getAllByUser(user.getId());
+        model.addAttribute("subscriptions", allByUser);
         return "user/info";
     }
 

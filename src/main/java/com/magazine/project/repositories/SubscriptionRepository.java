c7a0aa12ecf5400d9ef73eb332d3d7b071e4d346
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-11-16 21:20:13 +0200

    add getAllByUser

--- src/main/java/com/magazine/project/repositories/SubscriptionRepository.java
+++ src/main/java/com/magazine/project/repositories/SubscriptionRepository.java
@@ -1,6 +1,7 @@
 package com.magazine.project.repositories;
 
 import com.magazine.project.entity.Subscription;
+import com.magazine.project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-
+    List<Subscription> getAllByUser(User user);
     List<Subscription> getBySubscriptionExpDateAfter(LocalDateTime subscriptionExpDate);
 }

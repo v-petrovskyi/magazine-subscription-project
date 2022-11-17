4748e3aab569f516304060b94363bbb54f87495c
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-11-17 14:50:34 +0200

    add fields

--- src/main/java/com/magazine/project/entity/UserInfo.java
+++ src/main/java/com/magazine/project/entity/UserInfo.java
@@ -5,10 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Objects;
 
 @Getter
@@ -22,4 +20,36 @@ public class UserInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserInfo userInfo = (UserInfo) o;
+        return id == userInfo.id && Objects.equals(birthDate, userInfo.birthDate) && Objects.equals(name, userInfo.name) && Objects.equals(surname, userInfo.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, birthDate, name, surname);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("UserInfo{");
+        sb.append("id=").append(id);
+        sb.append(", birthDate=").append(birthDate);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", surname='").append(surname).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }

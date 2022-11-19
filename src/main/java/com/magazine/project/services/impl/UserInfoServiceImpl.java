a4e62ae6fee7e1937a9095a018f90baad88335ef
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-11-19 15:49:40 +0200

    add logs

--- src/main/java/com/magazine/project/services/impl/UserInfoServiceImpl.java
+++ src/main/java/com/magazine/project/services/impl/UserInfoServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityNotFoundException;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+
 @Slf4j
 @Service
 public class UserInfoServiceImpl implements UserInfoService {
@@ -22,6 +23,8 @@ public class UserInfoServiceImpl implements UserInfoService {
 
     @Override
     public boolean add(UserInfo userInfo) {
+        log.info("method add(UserInfo userInfo)");
+        log.info(userInfo.toString());
         UserInfo saved = userInfoRepository.save(userInfo);
         try {
             saved.getId();
@@ -33,22 +36,26 @@ public class UserInfoServiceImpl implements UserInfoService {
 
     @Override
     public UserInfo getById(long id) {
+        log.info("method getById(long id), id = {}", id);
         Optional<UserInfo> optionalUserInfo = userInfoRepository.findById(id);
         try {
             return optionalUserInfo.orElseThrow();
         } catch (NoSuchElementException e) {
+            log.error("UserInfo with id " + id + " not found");
             throw new EntityNotFoundException("UserInfo with id " + id + " not found");
         }
     }
 
     @Override
     public boolean update(UserInfo userInfo) {
+        log.info("method update(UserInfo userInfo)");
+        log.info(userInfo.toString());
         return add(userInfo);
     }
 
     @Override
     public boolean delete(long id) {
-        log.info("delete user id = {}",id);
+        log.info("method delete(long id), id = {}", id);
         userInfoRepository.delete(getById(id));
         return userInfoRepository.findById(id).isEmpty();
     }

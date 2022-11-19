2cfec9ca64c0b0c830c6a646e27a85194ae211b6
Volodymyr <volodymyr.petrovskyi@gmail.com>
2022-11-19 15:25:57 +0200

    create WrongTermException

---
+++ src/main/java/com/magazine/project/exception/WrongTermException.java
@@ -0,0 +1,7 @@
+package com.magazine.project.exception;
+
+public class WrongTermException extends RuntimeException{
+    public WrongTermException(String message) {
+        super(message);
+    }
+}
